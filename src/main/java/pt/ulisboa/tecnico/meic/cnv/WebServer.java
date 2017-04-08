8c6a44dac29f45cea7c65550351e5848b7754ad1
Francisco Santos <franciscopolaco@ist.utl.pt>
2017-04-08 13:45:09 +0100

    Call to Raytrace complete

--- src/main/java/pt/ulisboa/tecnico/meic/cnv/WebServer.java
+++ src/main/java/pt/ulisboa/tecnico/meic/cnv/WebServer.java
@@ -4,9 +4,11 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
 import java.util.TreeMap;
 
 public class WebServer {
@@ -18,6 +20,7 @@ public class WebServer {
         server.start();
     }
 
+
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
@@ -26,17 +29,11 @@ public class WebServer {
         }
     }
 
-    private static void writeResponse(HttpExchange t, String response) throws IOException {
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
-    }
-
-    static class BestThread extends Thread{
+    static class BestThread extends Thread {
         private HttpExchange httpExchange;
         private TreeMap<String, String> parameterMap;
 
-        BestThread(HttpExchange httpExchange){
+        BestThread(HttpExchange httpExchange) {
             this.httpExchange = httpExchange;
             parameterMap = new TreeMap<>();
         }
@@ -45,20 +42,43 @@ public class WebServer {
         public void run() {
             String query = httpExchange.getRequestURI().getQuery();
             String[] args = query.split("&");
-            for(String arg : args){
+            for (String arg : args) {
                 String[] parameters = arg.split("=");
-                if(parameters.length == 2){
+                if (parameters.length == 2) {
                     parameterMap.put(parameters[0], parameters[1]);
                 }
             }
-            String response = "Query Map :\n" + parameterMap.toString()
-                    + "##";
+
+            String outputFilename = parameterMap.get("f").replace(".txt", ".bmp");
+
+            String[] argsToRaytrace = new String[]{parameterMap.get("f"),
+                    outputFilename,
+                    parameterMap.get("sc"),
+                    parameterMap.get("sr"),
+                    parameterMap.get("wc"),
+                    parameterMap.get("wr"),
+                    parameterMap.get("coff"),
+                    parameterMap.get("roff")};
             try {
-                httpExchange.sendResponseHeaders(200, response.length());
-                writeResponse(httpExchange, response);
+                raytracer.Main.main(argsToRaytrace);
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+
+            File file = new File(outputFilename);
+            try {
+                httpExchange.sendResponseHeaders(200, file.length());
+                writeResponse(httpExchange, file);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        private void writeResponse(HttpExchange t, File file) throws IOException {
+            OutputStream os = t.getResponseBody();
+            Files.copy(file.toPath(), os);
+            //os.write(response.getBytes());
+            os.close();
         }
     }
 
