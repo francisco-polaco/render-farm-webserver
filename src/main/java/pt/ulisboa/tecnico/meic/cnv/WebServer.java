6bcc0ebb04a611df41c74014c2f19f998cc0cb29
Francisco Santos <franciscopolaco@ist.utl.pt>
2017-04-11 16:18:06 +0100

    Code Reformat

--- src/main/java/pt/ulisboa/tecnico/meic/cnv/WebServer.java
+++ src/main/java/pt/ulisboa/tecnico/meic/cnv/WebServer.java
@@ -3,14 +3,11 @@ package pt.ulisboa.tecnico.meic.cnv;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 import javax.imageio.ImageIO;
-import javax.sound.midi.SysexMessage;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -27,13 +24,13 @@ public class WebServer {
         server.createContext("/r.html", new MyHandler());
         server.setExecutor(null); // creates a default executor
         bindEnterToStop();
-        System.out.println("WebServer is running in port " + PORT);
+        System.out.println("Web Server is running on port " + PORT);
         System.out.println("Press <Enter> to stop.");
         server.start();
     }
 
     private static void bindEnterToStop() {
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 new Scanner(System.in).nextLine();
@@ -66,7 +63,7 @@ public class WebServer {
             String outputFilename = parameterMap.get("f").replace(".txt", ".bmp");
             try {
                 File f = new File(outputFilename);
-                if(f.exists()) {
+                if (f.exists()) {
                     BufferedImage bimg = ImageIO.read(f);
                     if (canBeResized(bimg)) {
                         System.out.println("I have a bigger file in cache, let's resize it.");
@@ -77,13 +74,13 @@ public class WebServer {
                         ImageIO.write(bufferedImage, "bmp", new File(newFilename));
                         dispatch(new File(newFilename));
                         return;
-                    }else if(hasSameSize(bimg)){
+                    } else if (hasSameSize(bimg)) {
                         System.out.println("I've found the file in cache.");
                         dispatch(f);
                         return;
                     }
                 }
-            }catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             String[] argsToRaytrace = new String[]{parameterMap.get("f"),
@@ -134,7 +131,7 @@ public class WebServer {
             }
         }
 
-        private BufferedImage resizeImage(BufferedImage originalImage, int type, int width, int height){
+        private BufferedImage resizeImage(BufferedImage originalImage, int type, int width, int height) {
             BufferedImage resizedImage = new BufferedImage(width, height, type);
             Graphics2D g = resizedImage.createGraphics();
             g.drawImage(originalImage, 0, 0, width, height, null);
