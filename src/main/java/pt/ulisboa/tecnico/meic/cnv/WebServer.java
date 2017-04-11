4eb2045379c2a827326521177dc432f4a2562544
Francisco Santos <franciscopolaco@ist.utl.pt>
2017-04-11 13:45:17 +0100

    optimizacao

--- src/main/java/pt/ulisboa/tecnico/meic/cnv/WebServer.java
+++ src/main/java/pt/ulisboa/tecnico/meic/cnv/WebServer.java
@@ -3,8 +3,13 @@ package pt.ulisboa.tecnico.meic.cnv;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -43,17 +48,23 @@ public class WebServer {
 
         @Override
         public void run() {
-            String query = httpExchange.getRequestURI().getQuery();
-            String[] args = query.split("&");
-            for (String arg : args) {
-                String[] parameters = arg.split("=");
-                if (parameters.length == 2) {
-                    parameterMap.put(parameters[0], parameters[1]);
-                }
-            }
-
+            parseRequest();
             String outputFilename = parameterMap.get("f").replace(".txt", ".bmp");
-
+            try {
+                File f = new File(outputFilename);
+                BufferedImage bimg = ImageIO.read(f);
+                if(bimg.getWidth() >= Integer.valueOf(parameterMap.get("sc")) &&
+                        bimg.getHeight() >= Integer.valueOf(parameterMap.get("sr")) &&
+                        bimg.getWidth() / bimg.getHeight() ==
+                                Integer.valueOf(parameterMap.get("sc")) / Integer.valueOf(parameterMap.get("sr"))){
+                    // a nossa imagem e maior ou igual do que estao a pedir e o msm ratio.
+                    System.out.println("OMG q FIXE!");
+                    dispatch(f);
+                    return;
+                }
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
             String[] argsToRaytrace = new String[]{parameterMap.get("f"),
                     outputFilename,
                     parameterMap.get("sc"),
@@ -67,13 +78,26 @@ public class WebServer {
             } catch (Throwable e) {
                 e.printStackTrace();
             }
+            dispatch(new File(outputFilename));
+        }
 
-            File file = new File(outputFilename);
+        private void dispatch(File file) {
             try {
                 httpExchange.sendResponseHeaders(200, file.length());
                 writeResponse(httpExchange, file);
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+
+        private void parseRequest() {
+            String query = httpExchange.getRequestURI().getQuery();
+            String[] args = query.split("&");
+            for (String arg : args) {
+                String[] parameters = arg.split("=");
+                if (parameters.length == 2) {
+                    parameterMap.put(parameters[0], parameters[1]);
+                }
             }
         }
 
