71c6596763ede5587eabb201bf064929266e2365
Francisco Santos <franciscopolaco@ist.utl.pt>
2017-05-08 12:24:51 +0100

    Test Page threaded

--- src/main/java/pt/ulisboa/tecnico/meic/cnv/TestHandler.java
+++ src/main/java/pt/ulisboa/tecnico/meic/cnv/TestHandler.java
@@ -9,12 +9,23 @@ import java.io.OutputStream;
 public class TestHandler implements HttpHandler{
 
     @Override
-    public void handle(HttpExchange t) throws IOException {
-        String response = "Page OK!";
-        t.sendResponseHeaders(200, response.length());
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
+    public void handle(final HttpExchange t) throws IOException {
+        new Thread(){
+            @Override
+            public void run() {
+                String response = "Page OK!";
+                try {
+                    t.sendResponseHeaders(200, response.length());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try (OutputStream os = t.getResponseBody()){
+                    os.write(response.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 
 }
